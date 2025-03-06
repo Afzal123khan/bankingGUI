6a6c169def90d9c245fb6d6130480965f3bad7d1
Afzal123khan <khanafzal416786@gmail.com>
2025-03-06 20:12:04 +0530

    Delete ErrorViewModel.cs

--- ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace VegWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
